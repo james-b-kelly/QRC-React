db10b8a1c3116cf8b9b3f64a34eb23fa6cc43e89
Jamie Kelly <james@builtforge.com>
2026-04-01 12:47:22 +0100

    QRC-34: Add Privacy Policy page with footer link

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Home from './pages/Home'
 import Editor from './pages/Editor'
 import Success from './pages/Success'
 import Cancel from './pages/Cancel'
+import Privacy from './pages/Privacy'
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/privacy" element={<Privacy />} />
       </Route>
       <Route element={<EditorLayout />}>
         <Route path="/editor" element={<Editor />} />
