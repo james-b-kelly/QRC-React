dd494d2ad25bc3312930b9754971cef00ce7f445
Jamie Kelly <james@builtforge.com>
2026-04-01 11:41:06 +0100

    QRC-33: Set document title on Terms page

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 export default function Terms() {
   useEffect(() => {
     window.scrollTo(0, 0)
+    document.title = 'Terms & Conditions | QR Studio'
   }, [])
 
   return (
